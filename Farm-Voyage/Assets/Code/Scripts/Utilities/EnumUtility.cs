35e0076ddc3720658667bcd39f3fdae244f34d41
Daniel Andrews <daniel.korienko@gmail.com>
2024-04-23 22:17:49 +0300

    Update enum utility

--- Farm-Voyage/Assets/Code/Scripts/Utilities/EnumUtility.cs
+++ Farm-Voyage/Assets/Code/Scripts/Utilities/EnumUtility.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Security.Cryptography;
 
 namespace Utilities
 {
-    public class EnumUtility
+    public static class EnumUtility
     {
-        private static readonly Random s_random = new();
+        private static readonly ConcurrentDictionary<Type, Array> s_enumValuesCache = new();
+        private static readonly RandomNumberGenerator s_rng = RandomNumberGenerator.Create();
 
         public static T GetRandomEnumValue<T>() where T : Enum
         {
-            Array values = Enum.GetValues(typeof(T));
-            int index = s_random.Next(values.Length);
+            Array values = s_enumValuesCache.GetOrAdd(typeof(T), t => Enum.GetValues(t));
+
+            byte[] buffer = new byte[4];
+            s_rng.GetBytes(buffer);
+            int index = Math.Abs(BitConverter.ToInt32(buffer, 0)) % values.Length;
+
             return (T)values.GetValue(index);
         }
     }
